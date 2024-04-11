53ebd986c4f4a5df2755fecc12a2f79d83258dd3
stephanidka <stephany.vakhotina@gmail.com>
2024-04-12 02:19:31 +0300

    structure

---
+++ src/components/Main.jsx
@@ -0,0 +1,18 @@
+// import ExampleCard from '../example_card'
+
+function Main(){
+    return(
+        <div className="MainContainer">
+            <input type="text" />
+            <div className="Card">
+                <img src="/example_card.png" alt="1" />
+            </div>
+            <div>table</div>
+            <button>push me</button>
+            <div>something important</div>
+            
+
+        </div>
+    )
+}
+export default Main
